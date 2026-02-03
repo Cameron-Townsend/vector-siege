6e0c0e4c842020477f50bf4efff78239946da060
Cameron-Townsend <cgt805@gmail.com>
2026-02-02 22:07:16 -0800

    Implement EventBus class for event handling

--- js/EventBus.js
+++ js/EventBus.js
@@ -1,1 +1,11 @@
-
+ class EventBus {
+            constructor() { this.events = {}; }
+            on(event, callback) {
+                if (!this.events[event]) this.events[event] = [];
+                this.events[event].push(callback);
+            }
+            emit(event, payload = {}) {
+                if (!this.events[event]) return;
+                this.events[event].forEach(callback => callback(payload));
+            }
+        }
